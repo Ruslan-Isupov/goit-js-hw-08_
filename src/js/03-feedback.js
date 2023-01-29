daf64dd9a4acbd97275739d6ba340edfa29779a6
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-01-29 15:11:13 +0200

    Resolve 3-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,48 +1,45 @@
-import  throttle from "lodash.throttle";
-const form = document.querySelector(".feedback-form")
-
-const LOCALSTORAGE_KEY = "feedback-form-state"
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 const userData = {
-    email: "",
-    message: "" ,
+  email: '',
+  message: '',
+};
+
+saveValueOfInput(userData);
+setValueOfUserData(userData);
+form.addEventListener('input', throttle(saveValueOfLocalStorage, 500));
+
+function saveValueOfLocalStorage(e) {
+
+setValueOfUserData(userData);
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(userData));
+  saveValueOfInput(userData);
+}
+function saveValueOfInput(userData) {
+  const gettingValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (gettingValue) {
+
+    form.elements.email.value = gettingValue.email;
+    form.elements.message.value = gettingValue.message;
+
+  }
+}
+function setValueOfUserData (){
+    userData.email = form.elements.email.value;
+    userData.message = form.elements.message.value;
 
 }
-setValueOfLocalStorage(userData)
-form.addEventListener('input', throttle(setValueOfUserData,500))
+form.addEventListener('submit', submitForm);
 
-
-function setValueOfUserData(e){
-    userData.email = form.elements.email.value
-    userData.message = form.elements.message.value
-    
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(userData))
-    setValueOfLocalStorage(userData)
-}    
-function setValueOfLocalStorage(userData){
-    const getValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY))
-
-    if(getValue){
-   form.elements.email.value = getValue.email
-   form.elements.message.value = getValue.message
-}
-}    
-form.addEventListener("submit", submitForm)
-function submitForm(e){ 
-   
-    if(
-    
-    form.elements.email.value === "" ||  form.elements.message.value  === ""
-)
-   { 
-    
-alert("You need to fill all fields")
-e.preventDefault() 
-   }
-   else {
-
-    console.log(userData)
-    e.preventDefault()
-    localStorage.removeItem(LOCALSTORAGE_KEY)
-    e.currentTarget.reset()
-   }
+function submitForm(e) {
+  if (form.elements.email.value === '' || form.elements.message.value === '') {
+    alert('You need to fill all fields');
+    e.preventDefault();
+  } else {
+    console.log(userData);
+    e.preventDefault();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    e.currentTarget.reset();
+  }
 }
